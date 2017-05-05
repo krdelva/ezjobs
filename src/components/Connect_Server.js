d04f5e3739978275bd1c16baa37fca6c07c54a00
Kevin <kevin.r.delva@gmail.com>
2017-05-04 18:59:02 -0700

    Trying to fix aoi connections

--- src/components/Connect_Server.js
+++ src/components/Connect_Server.js
@@ -2,13 +2,12 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://api.indeed.com/ads/apisearch?publisher=5101146230298050&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
 xhr.send(null);
 var weatherRes = undefined;
-xhr.onreadystatechange = () => {
+weatherRes = xhr.onreadystatechange = () => {
   let DONE = 4;
   let OK = 200;
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
       console.log(xhr.responseText);
-      //export weatherRes = xhr.responseText;
     } else {
       console.log('Error: ' + xhr.status);
     }
