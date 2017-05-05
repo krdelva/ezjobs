bb3437ac69237d7630ee0564196844e48872f9b6
Kevin <kevin.r.delva@gmail.com>
2017-05-04 19:10:30 -0700

    Keep them coming

--- src/components/Connect_Server.js
+++ src/components/Connect_Server.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.indeed.com/ads/apisearch?publisher=5101146230298050&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
+xhr.open('GET', 'https://api.indeed.com/ads/apisearch?publisher=5101146230298050&v=2');
 xhr.send(null);
 var weatherRes = undefined;
 weatherRes = xhr.onreadystatechange = () => {
