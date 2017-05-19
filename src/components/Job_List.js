ab5edf685d73f5e2463208abc7a39395107783c1
Kevin <kevin.r.delva@gmail.com>
2017-05-18 17:07:05 -0700

    Changes in item-list (Company/Location)

--- src/components/Job_List.js
+++ src/components/Job_List.js
@@ -21,7 +21,7 @@ class JobList extends Component {
             this.props.jobBool ? <JobDescription /> : this.props.data ? this.props.data.map((ele, i) => {
               return <div className='list_item' onClick={() => this.handleClick(ele.id)} key={i}>
                   <h3 style={{textDecoration:'underline', color: '#C7EFCF'}}>{ele.title}</h3>
-                  <p>{ele.company}</p><p>{ele.location}</p>
+                  <p>{ele.company} - <span style={{fontSize:'14px'}}>{ele.location}</span></p>
                 </div>
             }): this.props.loading ? <img className='center-img' src={require('../images/loading_gif_transparent.gif')} alt='Loading GIF' width='400' height='400' />
             : <h1 className='center' style={{color: '#C7EFCF'}}>Welcome to EZ JOBS</h1>
